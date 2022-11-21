8a9e1ab6d03036cde96d04fdeaac04965161eeb6
Temurbek Hamroyev <96319754+Timka0011@users.noreply.github.com>
2022-11-21 17:25:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+
+app.use(express.json());
+
+app.get("/download", (req, res) => {
+  const url = req.query.url;
+  res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+  ytdl(url, {
+    format: "mp4",
+  }).pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log("server on 3000");
+});
